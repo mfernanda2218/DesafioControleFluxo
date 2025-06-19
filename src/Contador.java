e8c89de52576974566ebaca0fc08defac12a8f99
MariaFernanda <mafaduartemelot216@gmail.com>
2025-06-19 13:43:26 -0300

    fix: ccorreção de condicional pra tratamento de erros na entrada

--- src/Contador.java
+++ src/Contador.java
@@ -5,25 +5,29 @@ public class Contador {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Informe um número:");
+        System.out.println("Informe o primeiro número:");
         int numMenor = scanner.nextInt();
 
-        System.out.println("Informe outro número que seja maior que o primeiro:");
+        System.out.println("Informe o segundo número (maior que o primeiro):");
         int numMaior = scanner.nextInt();
 
         try {
-            if (numMenor >= numMaior) {
-                throw new ParametrosInvalidosException("O segundo número deve ser maior que o primeiro.");
+            while (numMenor >= numMaior) {
+                System.out.println("O segundo número deve ser maior que o primeiro.");
+                System.out.println("Informe o segundo número (maior que o primeiro):");
+                numMaior = scanner.nextInt();
             }
             contar(numMenor, numMaior);
         } catch (ParametrosInvalidosException e) {
             System.out.println(e.getMessage());
         }
+        scanner.close();
     }
 
-    static void contar(int numMenor, int numMaior) {
-        for (int i = numMenor; i <= numMaior; i++) {
-            System.out.println(i);
+    static void contar(int numMenor, int numMaior) throws ParametrosInvalidosException {
+        int contagem = numMaior - numMenor;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo número " + i);
         }
     }
 }
